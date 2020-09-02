61d1e04625600ab75a44886457efdcfd188f188c
Assmodey32 <45710118+Assmodey32@users.noreply.github.com>
2020-09-02 19:31:15 +0300

    initial commit

---
+++ DotNet/Labs/Lab 1. Console Hello World!/Lab 1. Console Hello World!/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+
+                Console.WriteLine("Args is empty!");
+            else
+                Console.WriteLine("Hello, " + args[0]);
+        }
+    }
+}
