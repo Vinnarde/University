58febf0a9d13d3e070054b77a93ba81459d66cc3
Assmodey32 <45710118+Assmodey32@users.noreply.github.com>
2020-09-23 12:07:39 +0300

    lab 3 upd

---
+++ DotNet/Labs/Lab 4/Lab 4/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Lab_4
+{
+    public class Class1
+    {
+    }
+}
---
+++ DotNet/Labs/Lab 4/Lab 4/IsValidatable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_4
+{
+    interface IsValidatable
+    {
+
+    }
+}
